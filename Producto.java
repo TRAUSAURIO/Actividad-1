258e2be3204440d29a3cff4c5be2211725577c40
Ronald <76456440+TRAUSAURIO@users.noreply.github.com>
2025-11-18 16:41:24 -0500

    Add files via upload

---
+++ Producto.java
@@ -0,0 +1,47 @@
+public class Producto {
+    private String nombre;
+    private double precio;
+    private int cantidad;
+
+    public Producto(String nombre, double precio, int cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public double calcularTotal() {
+        return precio * cantidad;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("\nProducto: " + nombre);
+        System.out.println("Precio unitario: $" + precio);
+        System.out.println("Cantidad: " + cantidad);
+        System.out.println("Total: $" + calcularTotal());
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
